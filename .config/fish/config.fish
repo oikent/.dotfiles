b9b7fcea0834db109fc6c5c24ed68cb149fb9ac6
Christian <christianstickel@hotmail.com>
2022-08-15 09:42:51 +1000

    added fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,41 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# !!
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t $history[1]; commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+# !$
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t ""
+            commandline -f history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
+
+
+#aliases
+alias ls='ls -lah --color=auto'
+alias emacs="emacsclient -c -a 'emacs'"
+alias dot="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+
+#exports
+export PATH="$HOME/.emacs.d/bin:$PATH"
+
+
+starship init fish | source
